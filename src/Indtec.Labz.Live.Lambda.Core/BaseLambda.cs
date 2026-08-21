a4d1d632651260b9d6304fabb2f16e9fc168cfba
Felipe Fogaça <felipe.foogaca@gmail.com>
2026-08-20 23:46:10 -0300

    refactor: keep lambda container composition explicit

--- src/Indtec.Labz.Live.Lambda.Core/BaseLambda.cs
+++ src/Indtec.Labz.Live.Lambda.Core/BaseLambda.cs
@@ -5,11 +5,11 @@ namespace Indtec.Labz.Live.Lambda.Core;
 
 public abstract class BaseLambda<TRequest, TCommand, TResponse>
 {
-    private static readonly Lazy<IServiceProvider> RootProvider = new(BuildRootProvider, LazyThreadSafetyMode.ExecutionAndPublication);
+    protected abstract IServiceProvider Services { get; }
 
     public async Task<TResponse> FunctionHandler(TRequest request, ILambdaContext context)
     {
-        await using var scope = RootProvider.Value.CreateAsyncScope();
+        await using var scope = Services.CreateAsyncScope();
         var command = Map(request, context);
 
         try
@@ -30,36 +30,6 @@ public abstract class BaseLambda<TRequest, TCommand, TResponse>
         TCommand command,
         ILambdaContext context);
 
-    protected abstract void ConfigureServices(IServiceCollection services);
-
     protected virtual void OnUnhandledException(Exception exception, ILambdaContext context)
         => context.Logger.LogError($"Unhandled lambda exception. RequestId={context.AwsRequestId}. Error={exception}");
-
-    private static IServiceProvider BuildRootProvider()
-    {
-        var services = new ServiceCollection();
-        ConfigureSharedServices(services);
-        return services.BuildServiceProvider(validateScopes: true);
-    }
-
-    private static void ConfigureSharedServices(IServiceCollection services)
-    {
-        services.AddSingleton<LambdaRuntimeMarker>();
-        LambdaServiceRegistry.Configure<TRequest, TCommand, TResponse>(services);
-    }
-
-    private sealed class LambdaRuntimeMarker;
-
-    private static class LambdaServiceRegistry
-    {
-        private static Action<IServiceCollection>? _configure;
-
-        public static void Register(Action<IServiceCollection> configure) => _configure = configure;
-
-        public static void Configure<TReq, TCmd, TRes>(IServiceCollection services)
-            => _configure?.Invoke(services);
-    }
-
-    protected static void RegisterServices(Action<IServiceCollection> configure)
-        => LambdaServiceRegistry.Register(configure);
 }
